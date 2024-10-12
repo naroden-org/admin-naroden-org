f4171c454ff6be439d8e3241338ccda68eca0ca2
Martin Penkov <martin.penkov@eliseustech.com>
2024-10-12 18:22:48 +0300

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/admin-naroden-org' // TODO: remove me if not deployed in github pages
+  assetPrefix: '/admin-naroden-org/' // TODO: remove me if not deployed in github pages
 
 }
 
