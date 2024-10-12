ddbdba9ec38c04000ff603bc13bc314e8da349f6
Martin Penkov <martin.penkov@eliseustech.com>
2024-10-12 18:42:52 +0300

    remove asset prefix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/admin-naroden-org', // TODO: remove me if not deployed in github pages
-  assetPrefix: '/admin-naroden-org/' // TODO: remove me if not deployed in github pages
-
+  basePath: '/admin-naroden-org' // TODO: remove me if not deployed in github pages
 }
 
 module.exports = nextConfig
