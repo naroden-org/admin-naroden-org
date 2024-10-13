71d4ade752e362b4147b7833c79f159fe6c3376a
Martin Penkov <martin.penkov@eliseustech.com>
2024-10-13 13:14:31 +0300

    Change workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/admin-naroden-org' // TODO: remove me if not deployed in github pages
+  basePath: '/admin-naroden-org', // TODO: remove me if not deployed in github pages
+  output: "export" // <=== enables static exports
 }
 
 module.exports = nextConfig
